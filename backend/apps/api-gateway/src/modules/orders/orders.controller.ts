7e8573de8300101663b33d0552add5236cd4a4d8
Tuan Nguyen <tuan.nguyen@pito.vn>
2024-10-24 11:13:14 +0700

    Test husky + lint-staged show error

--- backend/apps/api-gateway/src/modules/orders/orders.controller.ts
+++ backend/apps/api-gateway/src/modules/orders/orders.controller.ts
@@ -19,8 +19,6 @@ export class OrdersController {
     @AuthUser() user: User,
   ) {
     const orders = await this.ordersService.getHistoryOrders(user.id, getListOrderDto);
-    const a = 123;
-    123;
 
     return orders;
   }
