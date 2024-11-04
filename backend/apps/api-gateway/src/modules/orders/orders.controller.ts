ec060aec915da76ac2610e909caac8f70dcf3ede
Tuan Nguyen <tuan.nguyen@pito.vn>
2024-11-04 11:19:55 +0700

    test: update version order to 1.0.27

--- backend/apps/api-gateway/src/modules/orders/orders.controller.ts
+++ backend/apps/api-gateway/src/modules/orders/orders.controller.ts
@@ -18,7 +18,7 @@ export class OrdersController {
 
   @Get('test')
   async test() {
-    await this.cacheManager.set('key', '1.0.26', 100 * 1000);
+    await this.cacheManager.set('key', '1.0.27', 100 * 1000);
 
     const result = await this.cacheManager.get('key');
 
