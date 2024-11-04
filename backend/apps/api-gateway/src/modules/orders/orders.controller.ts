cd3bfdd68fe1c22f1eb1377e22c847854a08cd56
Tuan Nguyen <tuan.nguyen@pito.vn>
2024-11-04 18:54:52 +0700

    chore: update cloudbuild.yaml

--- backend/apps/api-gateway/src/modules/orders/orders.controller.ts
+++ backend/apps/api-gateway/src/modules/orders/orders.controller.ts
@@ -18,7 +18,7 @@ export class OrdersController {
 
   @Get('test')
   async test() {
-    await this.cacheManager.set('key', '1.0.28', 100 * 1000);
+    await this.cacheManager.set('key', '1.0.29', 100 * 1000);
 
     const result = await this.cacheManager.get('key');
 
