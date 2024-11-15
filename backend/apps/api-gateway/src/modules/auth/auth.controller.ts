80b0ef01fead8dc0d136005bd7cf7e44bd2cb9d8
Tuan Nguyen <tuan.nguyen@pito.vn>
2024-11-15 16:13:32 +0700

    chore: api return sample response

--- backend/apps/api-gateway/src/modules/auth/auth.controller.ts
+++ backend/apps/api-gateway/src/modules/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { AuthUser } from '@gateway/decorators/auth-user.decorator';
+import { Auth } from '@gateway/decorators/http.decorator';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor() {}
+
+  @Auth()
+  @Get('user-info')
+  getProfile(@AuthUser() user: any) {
+    return { data: user };
+  }
 }
