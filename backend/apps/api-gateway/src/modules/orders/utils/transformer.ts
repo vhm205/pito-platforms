4c7344b209c212ce49749ce2ec65fd4f3026c632
Quang Gogh <quang.tran@pito.vn>
2024-12-20 15:31:27 +0700

    chore: fix filter between deliveryDate

--- backend/apps/api-gateway/src/modules/orders/utils/transformer.ts
+++ backend/apps/api-gateway/src/modules/orders/utils/transformer.ts
@@ -2,12 +2,16 @@ import { Order, OrderItem } from '@app/common';
 import { FilterRule, OrderStatus } from '@app/common/types/proto/common';
 import { IMAGE_BASE_URLS } from '@gateway/constants';
 import { generatePublicImageUrl } from '@gateway/utils/common';
+import { getBoundaryIsoStringForDay } from '@gateway/utils/datetime';
 
 export function transformFilterOrder(f: FilterRule) {
   if (f.column === 'search') f.column = 'orderCode';
   else if (f.column === 'status') {
     if (Number(f.value) === OrderStatus.PREPARED) f.column = 'operatorStatusCode';
     else f.column = 'statusCode';
+  } else if (f.column === 'deliveryDate') {
+    const [from, to] = f.value.split(',');
+    f.value = `${getBoundaryIsoStringForDay(from)},${getBoundaryIsoStringForDay(to, 'end')}`;
   }
 
   return f;
@@ -40,5 +44,9 @@ export function transformOrderItem(orderItem: OrderItem) {
 
 export function transformStoreOrderFilter(f: FilterRule) {
   if (f.column === 'status') f.column = 'statusCode';
+  else if (f.column === 'deliveryDate') {
+    const [from, to] = f.value.split(',');
+    f.value = `${getBoundaryIsoStringForDay(from)},${getBoundaryIsoStringForDay(to, 'end')}`;
+  }
   return f;
 }
