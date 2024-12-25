3b3df91b8c697046d505d455711db49ade206b9b
Nghia Tran <nghia.tran@pito.vn>
2024-12-25 18:41:18 +0700

    feat: add update menu item endpoint for operators

--- backend/apps/api-gateway/src/modules/menus/operator-menus.controller.ts
+++ backend/apps/api-gateway/src/modules/menus/operator-menus.controller.ts
@@ -1,25 +1,35 @@
 import { RoleType } from '@gateway/constants';
 import { ApiPageWrapperResponse, Auth } from '@gateway/decorators';
+import { ApiWrapperResponse } from '@gateway/decorators/api-wrapper-response.decorator';
 import { PageMetaDto } from '@gateway/gateway-common/dto/page-meta.dto';
 import { PageDto } from '@gateway/gateway-common/dto/page.dto';
 import { PartnerItemDto } from '@gateway/modules/menus/dtos/insert-menu-item.dto';
 import { OperatorQueryItemDto } from '@gateway/modules/menus/dtos/query-menu.dto';
+import { UpdateItemDto } from '@gateway/modules/menus/dtos/update-menu-item.dto';
+import { MenusService } from '@gateway/modules/menus/menus.service';
 import { OperatorMenusService } from '@gateway/modules/menus/operator-menus.service';
+import { DraftBypassPipe } from '@gateway/modules/menus/pipes/draft-bypass.pipe';
+import { UpdateItemSchema } from '@gateway/modules/menus/schemas/item.schema';
 import { emptyPaginationResponse } from '@gateway/utils/common';
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   HttpStatus,
   NotFoundException,
   Param,
+  Put,
   Query,
 } from '@nestjs/common';
 import { isEmpty } from 'lodash';
 
 @Controller('operator')
 export class OperatorMenusController {
-  constructor(private readonly service: OperatorMenusService) {}
+  constructor(
+    private readonly service: OperatorMenusService,
+    private readonly menusService: MenusService,
+  ) {}
 
   @Get('/stores/:storeId/items')
   @Auth([RoleType.OPERATOR])
@@ -53,5 +63,20 @@ export class OperatorMenusController {
     });
 
     return new PageDto(items, pageMeta);
+  }
+
+  @Put('items/:itemId')
+  @Auth([RoleType.OPERATOR])
+  @ApiWrapperResponse({ type: PartnerItemDto })
+  async updateMenuItem(
+    @Body(new DraftBypassPipe(UpdateItemSchema)) dto: Partial<UpdateItemDto>,
+    @Param('itemId') itemId: string,
+  ) {
+    const updatedItem = await this.menusService.updateMenuItem({
+      ...dto,
+      itemId,
+    });
+
+    return updatedItem;
   }
 }
